b5e7385c72443a746ec9cced4f2f4719e89d0274
Thierry Raimond <thierry.raimond@creativity-studios.com>
2022-09-30 17:57:32 +0200

    Updating README.MD file

--- src/examples/App.jsx
+++ src/examples/App.jsx
@@ -1,22 +1,39 @@
+import './App.scss'
 import { useState } from 'react'
 import { Datepicker } from '../lib'
 
 const App = () => {
   const [dateOfBirth, setDateOfBirth] = useState('')
-  const handleDateOfBirthInput = (date) => {
-    console.log('onChange', dateOfBirth, date)
-    setDateOfBirth(date)
+  const [startDate, setStartDate] = useState('')
+  const [submit, setSubmit] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmit(`
+    dateOfBirth:${dateOfBirth}
+    startDate:${startDate}`)
   }
 
   return (
-    <section>
-      <label htmlFor="date-of-birth">Date of Birth</label>
-      <Datepicker
-        id="date-of-birth"
-        onChange={handleDateOfBirthInput}
-        selected={dateOfBirth}
-        required
-      />
+    <section className="container">
+      <h1 className="title">Basic Datepicker React</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="date-of-birth">Date of Birth</label>
+        <Datepicker
+          id="date-of-birth"
+          onChange={(date) => setDateOfBirth(date)}
+          selected={dateOfBirth}
+          required
+        />
+        <label htmlFor="start-date">Start Date</label>
+        <Datepicker
+          id="start-date"
+          onChange={(date) => setStartDate(date)}
+          selected={startDate}
+          required
+        />
+        <button>Submit</button>
+      </form>
+      <pre>{submit}</pre>
     </section>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './app/store'
-// import App from './app/App'
 import App from './examples/App'
 import './index.scss'
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+root.render(<App />)
